b9afa472225ac08da2d22dabbf2f7764a1eb0476
ZeonLungPun <122013127+ZeonlungPun@users.noreply.github.com>
2024-06-09 17:22:47 +0800

    Add files via upload

---
+++ two_stream/SIFT_.py
@@ -0,0 +1,208 @@
+import os,cv2,random
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import torch.nn as nn
+import torchvision.models as models
+import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+
+class SIFTDescriptorDataset(Dataset):
+    def __init__(self, root_dir,new_size,saved_keypoints,transform=None):
+        self.root_dir=root_dir
+        self.transform=transform
+        self.new_size=new_size
+        self.video_paths = []
+        self.labels = []
+        self.class_map = {}
+        self.saved_keypoints=saved_keypoints
+        self.transform = transform
+
+        class_idx = 0
+        for class_name in os.listdir(self.root_dir):
+            class_folder = os.path.join(self.root_dir, class_name)
+            if os.path.isdir(class_folder):
+                self.class_map[class_name] = class_idx
+                class_idx += 1
+
+                for video_name in os.listdir(class_folder):
+                    self.video_paths.append(video_name)
+                    self.labels.append(self.class_map[class_name])
+
+    def __len__(self):
+        return len(self.video_paths)
+
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        path = self.video_paths[idx]
+        path = os.path.join(self.root_dir + '/' + list(self.class_map.keys())[label] + '/' + path)
+        cap = cv2.VideoCapture(path)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        # 隨機選擇一個 frame
+        random_frame = random.randint(0, total_frames - 1)
+
+        # 將 video 指針設定到隨機選擇的 frame
+        cap.set(cv2.CAP_PROP_POS_FRAMES, random_frame)
+
+        # 讀取 frame
+        ret, frame = cap.read()
+        height, width, _ = frame.shape
+        new_height, new_width = self.new_size
+        center_x, center_y = width // 2, height // 2
+        crop_x1 = center_x - new_width // 2
+        crop_y1 = center_y - new_height // 2
+        crop_x2 = center_x + new_width // 2
+        crop_y2 = center_y + new_height // 2
+        frame_cropped = frame[crop_y1:crop_y2, crop_x1:crop_x2]
+        #frame_cropped=cv2.fastNlMeansDenoisingColored(frame_cropped, None, h=10, templateWindowSize=7, searchWindowSize=21)
+        # img_yuv = cv2.cvtColor(frame_cropped, cv2.COLOR_BGR2YUV)
+        # # 應用 CLAHE 對 Y 通道（亮度）進行增強
+        # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
+        # img_yuv[:, :, 0] = clahe.apply(img_yuv[:, :, 0])
+        # # 將圖像轉換回 BGR 色彩空間
+        # frame_cropped = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+        # 在目標圖片上計算保存的關鍵點的描述子
+        _, des = sift.compute(frame_cropped, self.saved_keypoints)
+        if self.transform:
+            des=self.transform(des)
+
+        return des ,label
+
+
+img=cv2.imread('/home/punzeonlung/fog_paper/input/batch_1/frame_at_00_00_40.jpg')
+height, width, _ = img.shape
+sift = cv2.SIFT_create()
+new_size=(200,650)
+new_height, new_width = new_size
+center_x, center_y = width // 2, height // 2
+crop_x1 = center_x - new_width // 2
+crop_y1 = center_y - new_height // 2
+crop_x2 = center_x + new_width // 2
+crop_y2 = center_y + new_height // 2
+img = img[crop_y1:crop_y2, crop_x1:crop_x2]
+img = cv2.fastNlMeansDenoisingColored(img, None, h=10,  templateWindowSize=7, searchWindowSize=21)
+img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+# 應用 CLAHE 對 Y 通道（亮度）進行增強
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
+img_yuv[:, :, 0] = clahe.apply(img_yuv[:, :, 0])
+# 將圖像轉換回 BGR 色彩空間
+img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+kp1, des1 = sift.detectAndCompute(img,None)
+root_dir='./video_class'
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+dataset = SIFTDescriptorDataset(root_dir,new_size=new_size,saved_keypoints=kp1 ,transform=transform)
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+train_loader = DataLoader(train_dataset, batch_size=5, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=5, shuffle=False)
+
+for inputs, labels in train_loader:
+    print(inputs.shape)
+    print(labels)
+    break
+
+class ResNet18Custom(nn.Module):
+    def __init__(self, num_classes):  # 添加回歸輸出的數量
+        super(ResNet18Custom, self).__init__()
+        # 加載預設的resnet18模型
+        self.resnet18 = models.resnet50(pretrained=True)
+        # 修改第一層以接受20通道的輸入
+        self.resnet18.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=3, bias=False)
+        # self.resnet18.bn1 = nn.BatchNorm2d(64)
+        # 替換分類層以符合目標類別數量
+        num_ftrs = self.resnet18.fc.in_features
+
+        # 保存特徵提取部分
+        self.feature_extractor = nn.Sequential(*list(self.resnet18.children())[:-1])
+
+        # 定義分類分支
+        self.classification_branch = nn.Linear(num_ftrs, num_classes)
+
+
+
+    def forward(self, x):
+        # 提取特徵
+        x = self.feature_extractor(x)
+        x = x.view(x.size(0), -1)
+
+        # 分類輸出
+        class_output = self.classification_branch(x)
+
+        return class_output
+
+def train(model, train_loader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in train_loader:
+        inputs, labels= inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs= model(inputs)
+        loss = criterion(outputs, labels)
+        loss=loss
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * inputs.size(0)
+
+    epoch_loss = running_loss / len(train_loader.dataset)
+    return epoch_loss
+
+
+def test(model, test_loader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs, labels= inputs.to(device), labels.to(device)
+
+            outputs= model(inputs)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * inputs.size(0)
+            _, predicted = torch.max(torch.softmax(outputs,1), 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    epoch_loss = running_loss / len(test_loader.dataset)
+    accuracy = correct / total
+    return epoch_loss, accuracy
+
+
+# 創建模型實例
+model = ResNet18Custom(num_classes=3)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# lr=0.0001
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+scheduler = CosineAnnealingLR(optimizer, T_max=100, eta_min=0)
+
+# 設置追蹤最佳模型的變數
+best_accuracy = 0.0
+best_epoch = 0
+best_model_path = "best_model.pth"
+
+num_epochs = 100
+for epoch in range(num_epochs):
+    train_loss = train(model, train_loader, criterion, optimizer, device)
+    test_loss, test_accuracy= test(model, test_loader, criterion, device)
+    # 在每個epoch結束時更新學習率
+    #scheduler.step()
+    print(
+        f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f} ")
+    if test_accuracy> best_accuracy:
+        best_accuracy = test_accuracy
+        best_epoch = epoch
+        torch.save(model.state_dict(), best_model_path)
+        print(f"New best model saved with accuracy: {best_accuracy:.4f} at epoch {best_epoch + 1}")
---
+++ two_stream/siftnet.py
@@ -0,0 +1,198 @@
+import os,re
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import torch.nn as nn
+import torchvision.models as models
+import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score
+import torch.nn.functional as F
+
+num_frame=3
+class OpticalFlowDataset(Dataset):
+    def __init__(self, flow_root_dir, transform=None):
+        self.flow_root_dir = flow_root_dir
+        self.transform = transform
+        self.flow_paths = []
+        self.labels = []
+        self.class_map = {}
+        #self.csv=pd.read_csv('./output.csv')
+        #scaler = MinMaxScaler()
+        #self.csv.iloc[:,1]=scaler.fit_transform(np.array(self.csv.iloc[:,1]).reshape((-1,1)))
+        class_idx = 0
+        for class_name in os.listdir(flow_root_dir):
+            class_folder = os.path.join(flow_root_dir, class_name)
+            if os.path.isdir(class_folder):
+                self.class_map[class_name] = class_idx
+                class_idx += 1
+
+                for video_name in os.listdir(class_folder):
+                    self.flow_paths.append(video_name)
+                    self.labels.append(self.class_map[class_name])
+
+    def __len__(self):
+        return len(self.flow_paths)
+
+    def __getitem__(self, idx):
+        while True:
+            label = self.labels[idx]
+            flow_path = self.flow_paths[idx]
+            flow_path=os.path.join(self.flow_root_dir+'/'+list(self.class_map.keys())[label]+'/'+flow_path)
+            sub_flow_path_list=os.listdir(flow_path)
+            sub_flow_path_list = sorted(sub_flow_path_list, key=lambda x: int(re.findall(r'\d+', x)[0]))
+
+
+            # Check if there are 20 files
+            if len(sub_flow_path_list) < num_frame:
+                # Randomly select a new index
+                idx = np.random.randint(0, len(self.labels))
+                continue
+
+            flow=[]
+            for sub_flow_path in sub_flow_path_list:
+                final_path_=os.path.join(flow_path,sub_flow_path)
+                flow_=np.expand_dims(np.load(final_path_),2)
+                flow.append(flow_)
+
+            flow = np.concatenate(flow, axis=-1)
+
+
+
+            if self.transform:
+                flow = self.transform(flow)
+
+            return flow, label #torch.tensor(float(vis_num.iloc[0]),dtype=torch.float)
+
+
+class ResNet18Custom(nn.Module):
+    def __init__(self, num_classes):  # 添加回歸輸出的數量
+        super(ResNet18Custom, self).__init__()
+        # 加載預設的resnet18模型
+        self.resnet18 = models.resnet50(pretrained=True)
+        # 修改第一層以接受20通道的輸入
+        self.resnet18.conv1 = nn.Conv2d(num_frame, 64, kernel_size=3, stride=2, padding=3, bias=False)
+        # self.resnet18.bn1 = nn.BatchNorm2d(64)
+        # 替換分類層以符合目標類別數量
+        num_ftrs = self.resnet18.fc.in_features
+
+        # 保存特徵提取部分
+        self.feature_extractor = nn.Sequential(*list(self.resnet18.children())[:-1])
+
+        # 定義分類分支
+        self.classification_branch = nn.Linear(num_ftrs, num_classes)
+
+
+    def forward(self, x):
+        # 提取特徵
+        x = self.feature_extractor(x)
+        x = x.view(x.size(0), -1)
+
+        # 分類輸出
+        class_output = self.classification_branch(x)
+
+
+        return class_output
+
+# 设置光流数据目录
+flow_root_dir = './SIFT'
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+dataset = OpticalFlowDataset(flow_root_dir, transform=transform)
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+# 打印类别映射和数据集大小
+print(f"Class map: {dataset.class_map}")
+print(f"Total number of samples: {len(dataset)}")
+print(f"Train dataset size: {len(train_dataset)}")
+print(f"Test dataset size: {len(test_dataset)}")
+
+for inputs, labels in train_loader:
+    print(inputs.shape)
+    print(labels)
+
+    break
+
+def train(model, train_loader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in train_loader:
+        inputs, labels= inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs= model(inputs)
+        loss = criterion(outputs, labels)
+        loss=loss
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * inputs.size(0)
+
+    epoch_loss = running_loss / len(train_loader.dataset)
+    return epoch_loss
+
+
+def test(model, test_loader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs, labels= inputs.to(device), labels.to(device)
+
+            outputs= model(inputs)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * inputs.size(0)
+            _, predicted = torch.max(torch.softmax(outputs,1), 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    epoch_loss = running_loss / len(test_loader.dataset)
+    accuracy = correct / total
+    return epoch_loss, accuracy
+
+
+# 創建模型實例
+model = ResNet18Custom(num_classes=3)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# lr=0.0001
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+#scheduler = CosineAnnealingLR(optimizer, T_max=100, eta_min=0)
+
+# 設置追蹤最佳模型的變數
+best_accuracy = 0.0
+best_epoch = 0
+best_model_path = "best_model.pth"
+
+num_epochs = 300
+for epoch in range(num_epochs):
+    train_loss = train(model, train_loader, criterion, optimizer, device)
+    test_loss, test_accuracy= test(model, test_loader, criterion, device)
+    # 在每個epoch結束時更新學習率
+    #scheduler.step()
+    print(
+        f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f} ")
+    if test_accuracy> best_accuracy:
+        best_accuracy = test_accuracy
+        best_epoch = epoch
+        torch.save(model.state_dict(), best_model_path)
+        print(f"New best model saved with accuracy: {best_accuracy:.4f} at epoch {best_epoch + 1}")
+
+
